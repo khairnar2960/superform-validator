5f36e16ba530b935b17b41bcdee8acf6eb7b9007
Harshal Khairnar <khairnar2960@gmail.com>
2025-07-01 19:51:45 +0530

    fix(schemaParser): validation function will be return for matching rule

--- src/core/schema-parser.ts
+++ src/core/schema-parser.ts
@@ -2,15 +2,14 @@
 
 import { extractParam } from "./param-extractor.js";
 import { parseParam } from "./param-parser.js";
-import { Processor } from "./processor.js";
+import { ProcessorFunc } from "./processors/processor.js";
 import { resolveRule } from "./rule-registry.js";
-import { BaseRule } from "./rules/base-rule.js";
+import { RuleFunction } from "./rules/base-rule.js";
 
 export type FieldRule = {
     name: string,
-    func: string | null,
     type: string,
-    rule: BaseRule|Processor|null,
+    rule: RuleFunction|ProcessorFunc|null,
     param: any;
     message?: string
 };
@@ -27,10 +26,10 @@ export function parseSchema(rawSchema: any): ParsedSchema {
         if (typeof schemaDef === 'string') {
             const rules = schemaDef.split(/\|(?![^(]*\))/).map(rule => rule.trim());
             rules.forEach(ruleStr => {
-                const { name, func, param: rawParam } = extractParam(ruleStr);
-                const { rule, type, paramType, argumentType } = resolveRule(name);
+                const { name, param: rawParam } = extractParam(ruleStr);
+                const { function: validator, type, paramType, argumentType } = resolveRule(name);
                 const param = parseParam(rawParam, paramType, argumentType);
-                fieldRules.push({ name, func, type, rule, param });
+                fieldRules.push({ name, type, rule: validator, param });
             });
         }
 
@@ -44,15 +43,14 @@ export function parseSchema(rawSchema: any): ParsedSchema {
                 // Custom rule handling
                 if (typeof param === 'object' && (param.callback || param.pattern)) {
                     fieldRules.push({
-                        name: ruleName, func: (param.callback ? 'callback' : 'pattern'),
+                        name: ruleName,
                         type: 'custom', rule: null, param
                     });
                 } else {
-                    const [_,func = null] = ruleName.split('::').map(n => n.trim());
-                    const { rule, type, paramType, argumentType } = resolveRule(ruleName);
+                    const { function: validator, type, paramType, argumentType } = resolveRule(ruleName);
                     const parsedParam = parseParam(param === true ? null : String(param), paramType, argumentType);
                     const message = messages[ruleName];
-                    fieldRules.push({ name: ruleName, func, type, rule, param: parsedParam, message });
+                    fieldRules.push({ name: ruleName, type, rule: validator, param: parsedParam, message });
                 }
             }
         }
