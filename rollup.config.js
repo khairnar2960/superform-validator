fbac25139d2a1125d3144ae54acc0c0b2c090ab0
Harshal Khairnar <khairnar2960@gmail.com>
2025-07-03 21:32:44 +0530

    fix(build): fixes umd build including minified

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,20 @@
 import terser from "@rollup/plugin-terser";
+import { version } from "./package.json";
 
 export default {
-    input: "dist/index.js",
+    input: "dist/core/validator.js",
     output: [
         {
             file: "dist/form-validator.js",
             format: "umd",
             name: "FormValidator",
+            footer: `FormValidator.version = '${version}';`,
         },
         {
             file: "dist/form-validator.min.js",
             format: "umd",
             name: "FormValidator",
+            footer: `FormValidator.version = '${version}';`,
             plugins: [terser()],
         },
     ],
