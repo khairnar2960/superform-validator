e94aa5b40faba39a3586692f37da23c97332b253
Harshal Khairnar <khairnar2960@gmail.com>
2025-11-15 01:20:54 +0530

    feat: adds additional core rules
    - isBoolean
    - isNumber
    - isInteger
    - isFloat
    - isArrayOf

--- src/core/rules/core-rules.ts
+++ src/core/rules/core-rules.ts
@@ -2,7 +2,7 @@ export const isString = (value: any): boolean => 'string' === typeof value;
 
 export const isArray = (value: any): boolean => Array.isArray(value);
 
-export const isObject = (value: any): boolean => null !== value && 'object' === typeof value && !isArray(value);
+export const isObject = (value: any): boolean => null !== value && !isArray(value) && 'object' === typeof value;
 
 export const isEmpty = (value: any): boolean => {
     if (isString(value) || isArray(value)) return value.length === 0;
@@ -19,3 +19,48 @@ export const isJson = (value: string): boolean => {
         return false;
     }
 }
+
+export const isBoolean = (value: any) => 'boolean' === typeof value;
+
+export const isNumber = (value: any): boolean => 
+    'number' === typeof value && Number.isFinite(value);
+
+export const isInteger = (value: any): boolean => Number.isInteger(value);
+
+export const isFloat = (value: any): boolean =>
+    isNumber(value) && !Number.isInteger(value);
+
+export type TypeOfArray = 'string'|'number'|'integer'|'float'|'boolean'|'array'|'object';
+
+export const isArrayOf = (input: any, type: TypeOfArray): boolean => {
+    if (!isArray(input)) return false;
+    for (const el of input) {
+        switch (type) {
+            case 'string':
+                if (!isString(el)) return false;
+                break;
+            case 'number':
+                if (!isNumber(el)) return false;
+                break;
+            case 'boolean':
+                if (!isBoolean(el)) return false;
+                break;
+            case 'integer':
+                if (!isInteger(el)) return false;
+                break;
+            case 'float':
+                if (!isFloat(el)) return false;
+                break;
+            case 'array':
+                if (!isArray(el)) return false;
+                break;
+            case 'object':
+                if (!isObject(el)) return false;
+                break;
+            default:
+                return false;
+        }
+    }
+    return true;
+}
+
