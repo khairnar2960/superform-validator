d0cc52d0c81576a906c30f5851f5141cb490f42a
Harshal Khairnar <khairnar2960@gmail.com>
2025-11-22 22:19:32 +0530

    fix: Removes plugins entry from index
    BREAKING CHANGE: Using in react or express only missing dependency error resolved
    >Use directly from superform-validator/react or superform-validator/express

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
 export { registerRule } from "./core/rule-registry.js";
 export { parseSchema } from "./core/schema-parser.js";
 export { validateField, validate } from "./core/validator-engine.js";
-import validator from "./middlewares/express.js";
-export { validator as expressValidator };
-export { default as reactValidator } from "./middlewares/react.js";
