df6750c5e821e89f83e4b4a9a7d763fe0796c151
Harshal Khairnar <khairnar2960@gmail.com>
2026-07-19 09:21:16 +0530

    fix: fixes packaging for esm

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,76 @@
 import terser from "@rollup/plugin-terser";
 import { version } from "./package.json";
 
-export default {
-    input: "dist/core/validator.js",
-    output: [
-        {
-            file: "dist/form-validator.js",
-            format: "umd",
-            name: "FormValidator",
-            footer: `FormValidator.version = '${version}';`,
-        },
-        {
-            file: "dist/form-validator.min.js",
-            format: "umd",
-            name: "FormValidator",
-            footer: `FormValidator.version = '${version}';`,
-            plugins: [terser()],
-        },
-    ],
-};
+export default [
+    // Package builds
+    {
+        input: "dist/core/validator.js",
+
+        output: [
+            {
+                file: "dist/index.js",
+                format: "esm",
+            },
+            {
+                file: "dist/index.cjs",
+                format: "cjs",
+                exports: "named",
+            },
+        ],
+    },
+
+    // middleware builds
+    // express
+    {
+        input: "dist/middlewares/express.js",
+
+        output: [
+            {
+                file: "dist/middlewares/express.js",
+                format: "esm",
+            },
+            {
+                file: "dist/middlewares/express.cjs",
+                format: "cjs",
+                exports: "named",
+            },
+        ],
+    },
+
+    // react
+    {
+        input: "dist/middlewares/react.js",
+
+        output: [
+            {
+                file: "dist/middlewares/react.js",
+                format: "esm",
+            },
+            {
+                file: "dist/middlewares/react.cjs",
+                format: "cjs",
+                exports: "named",
+            },
+        ],
+    },
+
+    // Browser UMD builds
+    {
+        input: "dist/core/validator.js",
+        output: [
+            {
+                file: "dist/form-validator.js",
+                format: "umd",
+                name: "FormValidator",
+                footer: `FormValidator.version = '${version}';`,
+            },
+            {
+                file: "dist/form-validator.min.js",
+                format: "umd",
+                name: "FormValidator",
+                footer: `FormValidator.version = '${version}';`,
+                plugins: [terser()],
+            },
+        ],
+    },
+];
